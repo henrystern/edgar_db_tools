6d0ff7a2d8207ed896a20623ec0dac1b4ed71f30
hstern <h@hstern.ca>
2023-01-30 10:46:22 -0500

    clean up price data collection

--- Edgar_DB_Tools/get_price_data.py
+++ Edgar_DB_Tools/get_price_data.py
@@ -1,45 +1,40 @@
 import yfinance as yf
 import pandas as pd
 from datetime import datetime
-import time
+import os.path
+from ratelimit import limits, sleep_and_retry
 
 # Get Yahoo Finance OHLCV for each ticker
-def get_price_data(start=datetime(2009, 1, 1).strftime('%Y-%m-%d'), 
-                   end=datetime.today().strftime('%Y-%m-%d'), 
-                   frequency='1wk', # see yfinance docs 
-                   cik_to_tick=pd.read_json('../other_data/CIK_to_company_tickers.json'), 
-                   output_dir='../other_data/'):
+def get_price_data(start=datetime(2009, 1, 1).strftime('%Y-%m-%d'),
+                   end=datetime.today().strftime('%Y-%m-%d'),
+                   frequency='1wk',  # see yfinance docs
+                   data='./other_data/CIK_to_company_tickers.json',
+                   output_dir='./other_data/',
+                   output_name='price_data.csv'):
 
-    cik_to_tick = cik_to_tick[(cik_to_tick.company_name != "''")]
+    cik_to_tick = pd.read_json(data).T
+    num_tickers = len(cik_to_tick) - 1
+    for i, (cik, ticker, company) in cik_to_tick.iterrows():
+        print(f"{i + 1} out of {num_tickers}:", company, ticker)
 
-    price = pd.DataFrame()
+        # max 2000 requests per hour
+        check_api_limit()
 
-    progress = 1
+        stock_data = yf.download(ticker, start, end, interval=frequency)
+        stock_data.insert(0, 'Ticker', ticker)
+        stock_data.insert(0, 'Company', company)
+        stock_data.insert(0, 'CIK', cik)
 
-    num_tickers = len(cik_to_tick)
+        if not os.path.exists(output_dir + output_name):
+            stock_data.to_csv(output_dir + output_name, header=True)
+        else:
+            stock_data.to_csv(output_dir + output_name, header=False, mode="a")
 
-    for i in range(0, num_tickers):
-        ticker = cik_to_tick.ticker.iloc[i]
-        print(ticker)
-        if (i/len(cik_to_tick))*100 > progress:
-            print(progress, '%')
-            progress = progress + 1
-            if progress % 10 == 0:
-                # Save some progress in case there's an error
-                price.to_csv(output_dir + 'price_data.csv', index=False)
-        raw_data = yf.download(ticker, start, end, interval=frequency)
-        stock_data = pd.DataFrame(
-            columns=['ticker', 'cik', 'date', 'open', 'high', 'low', 'close', 'volume'])
-        stock_data.price = raw_data.Close
-        stock_data.ticker = ticker
-        stock_data.date = raw_data.index
-        stock_data.volume = raw_data.Volume
-        price = price.append(stock_data)
-        time.sleep(2)  # max 2000 requests per hour
 
-    # 10,000 tickers will take 5.5 hours to complete
-
-    price.to_csv(output_dir + 'price_data.csv', index=False)
+@sleep_and_retry
+@limits(calls=2000, period=3600)
+def check_api_limit():
+    ...
 
 if __name__ == "__main__":
     get_price_data()
